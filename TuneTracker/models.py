9c1e7cf1e017007d254a5d4d83aca81398d61d38
Tushar Sahani <tusharsahani000@gmail.com>
2024-01-13 15:29:35 +0530

    models created

---
+++ TuneTracker/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+# Artist Model
+class Artist(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+#Song Model
+    
+class Song(models.Model):
+    title = models.CharField(max_length=30)
+    name = models.ForeignKey(Artist ,on_delete=models.CASCADE,null = False)
+
+    def __str__(self):
+        return self.title
